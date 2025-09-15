339e5f01a8eabaf58ca2dac2526efe93ce3ede08
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-09-15 09:30:28 +0700

    feat: add answers to qn-3

--- question-3.js
+++ question-3.js
@@ -1,6 +1,7 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
+O(n) because it is a linear search where it checks every element one by one until you find the target.
 
 
 */
@@ -8,6 +9,7 @@ Answer:
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
+O(log n) because it is a binary search where it repeatedly divide array in half and ignore the half where the target is not there.
 
 
 */
@@ -15,6 +17,7 @@ Answer:
 /* 
 Which function is more efficient and why?
 Answer:
+findProductPrice is more efficient because it removes the elements in the array by half and do not check the half where the target cannot be instead of looping through one by one.
 
 
 */
