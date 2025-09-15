bac451c4aa987f1e098618f5d21136e9531b9c76
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-09-15 09:48:30 +0700

    feat: add answer to qn-5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,10 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
+O(n^2) because it is a nested loop where the time complexity of each loop is O(n),
+therefore you multiply them when they are nested and get O(n^2).
+For O(n^2), which is the quadratic time complexity, the algorithm increases as the input size increases.
+
 
 
 */
